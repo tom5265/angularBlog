4663e9153311c54aa288c7f71cda4732c7acec10
Tom Moore <tom5265@gmail.com>
2016-03-18 09:59:53 -0500

    added page for singlepost

--- client/js/factories.js
+++ client/js/factories.js
@@ -1,6 +1,14 @@
 //The action to call the local API should be here
 
-var app = angular.module('', ['']);
-app.factory('', ['', []]);
-
+var factories = angular.module('myApp.factories', []);
+factories.factory('WordFactory', ['$http', function($http) {
+    var f = {};
+    f.age = 23;
+    
+    f.sayHello = function() {
+        console.log('Hello World!');
+    }
+    
+    return f;
+}]);
 
