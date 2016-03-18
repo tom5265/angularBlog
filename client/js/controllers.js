46287638783c869ffb0ad9cecc33e67e5c84fc09
Tom Moore <tom5265@gmail.com>
2016-03-18 13:24:52 -0500

    saving progress

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -3,13 +3,23 @@
 var controllers = angular.module('myApp.controllers', []);
 
 controllers.controller('postViewController', ['$scope', 'PostGetter', function($scope, PostGetter) {
-   
+
     $scope.getPosts = function() {
-        PostGetter.query().then(function(posts) {
-            $scope.posts = posts;
+        PostGetter.query().then(function(data) {
+            $scope.posts = data;
         })
+        $scope.getPosts();
     }
-    
 }]);
 
 
+controllers.controller('newPostController', ['$scope', 'PostGetter', function ($scope, PostGetter) {
+    $scope.addPost = function() {
+        var newPost = {
+            title: $scope.titleInput,
+            author: $scope.authorInput,
+            content: $scope.contentInput
+        }
+        PostGetter.post(newPost);
+    }
+}])
